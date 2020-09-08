2f79440c49ef90c9678ad860f025fb5e239d8276
Eva van Geenen <evavangeenen@gmail.com>
2020-09-08 12:19:07 +0200

    Added spring-boot-starter-validation dependency

--- validatingFormInput/src/main/java/eva/springguides/validatingFormInput/WebController.java
+++ validatingFormInput/src/main/java/eva/springguides/validatingFormInput/WebController.java
@@ -1,4 +1,41 @@
 package eva.springguides.validatingFormInput;
 
-public class WebController {
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.validation.Valid;
+
+@Controller
+public class WebController implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/results").setViewName("results");
+    }
+
+
+    @GetMapping("/")
+    public String showForm(PersonForm personForm){
+        return "form";
+    }
+
+    @PostMapping("/")
+    public String checkPersonInfo(@Valid PersonForm personForm,
+                                  BindingResult bindingResult){
+
+        System.out.println("This is the name of the bindingResult: "
+                    + bindingResult.getRawFieldValue("name"));
+
+        System.out.println("BindingResult has errors yes or no: " +
+                bindingResult.hasErrors());
+        if (bindingResult.hasErrors()){
+            return "form";
+        }
+        return "redirect:/results";
+    }
+
 }
