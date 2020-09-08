5b917322143f426a875de1b38da642b0bceca27c
Eva van Geenen <evavangeenen@gmail.com>
2020-09-08 13:24:17 +0200

    First Commit Securing A Web Application

---
+++ SecuringAWebApplication/src/main/java/eva/springguides/SecuringAWebApplication/SecuringAWebApplication.java
@@ -0,0 +1,13 @@
+package eva.springguides.SecuringAWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringAWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringAWebApplication.class, args);
+	}
+
+}
