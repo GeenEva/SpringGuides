5f391e3f26dc6de32b7547d9aaa81e088c96def7
Eva van Geenen <evavangeenen@gmail.com>
2020-09-08 10:55:11 +0200

    First Commit Validating Form Input.

--- handlingFormSubmission/src/main/java/eva/springguides/handlingFormSubmission/HandlingFormSubmissionApplication.java
+++ handlingFormSubmission/src/main/java/eva/springguides/handlingFormSubmission/HandlingFormSubmissionApplication.java
@@ -2,13 +2,16 @@ package eva.springguides.handlingFormSubmission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HandlingFormSubmissionApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(HandlingFormSubmissionApplication.class, args);
+	SpringApplication.run(HandlingFormSubmissionApplication.class, args);
 	}
 
 }
---
+++ validatingFormInput/src/main/java/eva/springguides/validatingFormInput/ValidatingFormInputApplication.java
@@ -0,0 +1,13 @@
+package eva.springguides.validatingFormInput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatingFormInputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatingFormInputApplication.class, args);
+	}
+
+}
