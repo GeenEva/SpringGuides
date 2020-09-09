4a8c35774e15aba03057b32f906060b98f9b4769
Eva van Geenen <evavangeenen@gmail.com>
2020-09-09 10:49:32 +0200

    Websecurity with encoded password

--- SecuringAWebApplication/src/main/java/eva/springguides/SecuringAWebApplication/WebSecurityConfig.java
+++ SecuringAWebApplication/src/main/java/eva/springguides/SecuringAWebApplication/WebSecurityConfig.java
@@ -1,4 +1,59 @@
 package eva.springguides.SecuringAWebApplication;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "/home").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login").defaultSuccessUrl("/home")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+    }
+
+    private final String encodedPassword = "$2a$10$DIdUpaRS1J2x2zVeoGziLeJk7FDb4BU0.pFGTDXzAO5NUE/T0apmK";
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(passwordEncoder())
+                .withUser("user").password(encodedPassword).roles("USER");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    /*@Bean
+    @Override
+    public UserDetailsService userDetailsService(){
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }*/
 }
